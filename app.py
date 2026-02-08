6baa0a13aebdb4d9d7cc24de61d991a4e1e4152a
sseptiannn <septianpratama18@gmail.com>
2026-02-08 09:52:08 +0700

    commit awalan aja dlu

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
-mport streamlit as st
+import streamlit as st
 import pandas as pd
-from src.preprocessing import DataPreprocessor
-from src.clustering import CustomerClustering
+# from src.preprocessing import DataPreprocessor
+# from src.clustering import CustomerClustering
 
 
 st.title("Customer Segmentation & Default Risk Analysis")
@@ -10,19 +10,19 @@ st.title("Customer Segmentation & Default Risk Analysis")
 uploaded_file = st.file_uploader("Upload customer data CSV")
 
 
-if uploaded_file:
-df = pd.read_csv(uploaded_file)
-pre = DataPreprocessor()
-df = pre.feature_engineering(df)
+# if uploaded_file:
+#     df = pd.read_csv(uploaded_file)
+#     pre = DataPreprocessor()
+#     df = pre.feature_engineering(df)
 
 
-features = ['payment_ratio', 'late_payment_frequency']
-X = pre.scale_features(df, features)
+#     features = ['payment_ratio', 'late_payment_frequency']
+#     X = pre.scale_features(df, features)
 
 
-cluster_model = CustomerClustering(n_clusters=3)
-df['cluster'] = cluster_model.fit_predict(X)
+#     cluster_model = CustomerClustering(n_clusters=3)
+#     df['cluster'] = cluster_model.fit_predict(X)
 
 
-st.write("### Clustered Customers")
-st.dataframe(df)
+# st.write("### Clustered Customers Test")
+# st.dataframe(df)
